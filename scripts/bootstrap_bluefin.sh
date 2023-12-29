6395a824fbf162c0fb9102c219041e1b4a16ab39
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2023-12-28 20:52:51 -0500

    [Bootstrap,Bluefin] Add bashrc config.

--- scripts/bootstrap_bluefin.sh
+++ scripts/bootstrap_bluefin.sh
@@ -62,3 +62,30 @@ ensure_commands() {
 # Check if necessary commands are available
 ensure_commands
 
+# Function to setup Bash
+setup_bashrc() {
+    # Define the directory and file path
+    bashrc_d_dir="$HOME/.bashrc.d"
+    bluefin_file="$bashrc_d_dir/bluefin"
+    
+    # Create the ~/.bashrc.d directory if it does not exist
+    if [[ ! -d "$bashrc_d_dir" ]]; then
+        mkdir -p "$bashrc_d_dir"
+        echo "Created directory: $bashrc_d_dir"
+    fi
+    
+    # Create and populate the bluefin file if it does not exist
+    if [[ ! -f "$bluefin_file" ]]; then
+        cat <<-EOF > "$bluefin_file"
+	# Add nix to PATH manually
+        export PATH=\$PATH:/nix/var/nix/profiles/default/bin
+	# Source Devbox Global
+        eval "\$(devbox global shellenv --init-hook)"
+EOF
+        echo "Created and initialized file: $bluefin_file"
+    else
+        echo "File already exists: $bluefin_file"
+    fi
+}
+
+setup_bashrc

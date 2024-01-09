80cd7932e327e3c009c8c6465484195773255507
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2024-01-09 15:57:35 -0800

    [Bootstrap,Devbox] Add bootstrap for devbox only.

---
+++ scripts/bootstrap_devbox.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+#
+# README ======================================
+#  # Run FIRST
+#  ```
+#  just devmode
+#  refresh-global
+#  systemctl reboot
+#  ```
+# =============================================
+
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# # Install Nix
+# if ! command_exists nix; then
+#     just nix-install
+# fi
+
+# Function to ensure necessary commands are available
+ensure_commands() {
+    local missing_cmds=()
+
+    if ! command_exists devbox; then
+        missing_cmds+=("devbox")
+    fi
+
+    # Handle missing commands
+    if [ ${#missing_cmds[@]} -ne 0 ]; then
+        echo "The following commands are not available but are required: ${missing_cmds[*]}"
+        echo "Please install them and rerun the script."
+        exit 1
+    fi
+}
+
+# Check if necessary commands are available
+ensure_commands
+
+# Function to setup Bash
+setup_bashrc() {
+    # Define the directory and file path
+    bashrc_d_dir="$HOME/.bashrc.d"
+    devbox_file="$bashrc_d_dir/devbox"
+
+    # Create the ~/.bashrc.d directory if it does not exist
+    if [[ ! -d "$bashrc_d_dir" ]]; then
+        mkdir -p "$bashrc_d_dir"
+        echo "Created directory: $bashrc_d_dir"
+    fi
+
+    # Create and populate the devbox file if it does not exist
+    if [[ ! -f "$devbox_file" ]]; then
+        cat <<-EOF > "$devbox_file"
+        # Add nix to PATH manually
+        export PATH=\$PATH:/nix/var/nix/profiles/default/bin
+        # Source Devbox Global
+        eval "\$(devbox global shellenv --init-hook)"
+EOF
+        echo "Created and initialized file: $devbox_file"
+    else
+        echo "File already exists: $devbox_file"
+    fi
+}
+
+setup_bashrc
+eval "$(devbox global shellenv --recompute)"

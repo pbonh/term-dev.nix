89d28a89d970a58d87a3b676d73877263f469573
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2024-04-15 01:37:59 +0000

    [Bluefin] Install github action runner.

--- scripts/bootstrap_bluefin.sh
+++ scripts/bootstrap_bluefin.sh
@@ -9,6 +9,9 @@
 #  ```
 # =============================================
 
+# Install Github Action Runner
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+
 # Install System Prerequisites
 rpm-ostree install ansible
 
@@ -17,41 +20,19 @@ command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-# Install Nix
-if ! command_exists nix; then
-    just nix-install
-    refresh-global
-fi
-
-# Install Devbox
-if ! command_exists devbox; then
-    just devbox-install
-    refresh-global
-    just nix-devbox
-    refresh-global
-    just nix-devbox-global
-    refresh-global
-fi
-
-# Install Fleek
-if ! command_exists fleek; then
-    just fleek-install
-    refresh-global
-fi
-
 # Function to ensure necessary commands are available
 ensure_commands() {
     local missing_cmds=()
 
     # Check for git and ghq
+    if ! command_exists ansible; then
+        missing_cmds+=("ansible")
+    fi
     if ! command_exists nix; then
         missing_cmds+=("nix")
     fi
     if ! command_exists devbox; then
         missing_cmds+=("devbox")
-    fi
-    if ! command_exists fleek; then
-        missing_cmds+=("fleek")
     fi
 
     # Handle missing commands
